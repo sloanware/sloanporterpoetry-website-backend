0ca5be7dd807ecdae65d589f84b9c4c38670d9b1
Sloan <sloanware@outlook.com>
2025-08-13 02:44:22 -0400

    Resolve syntax error.

--- middleware/rateLimit.js
+++ middleware/rateLimit.js
@@ -1,7 +1,7 @@
 import rateLimit from 'express-rate-limit';
 
 // Limits each IP to 3 requests per 1 minute window.
-const contactFormLimiter = rateLimit({
+export const contactFormLimiter = rateLimit({
     windowMs: 60000,
     max: 3,
     message: { msg: 'Too many submissions. Please try again later.' },
@@ -10,12 +10,10 @@ const contactFormLimiter = rateLimit({
 });
 
 // Limits each IP to 3 requests per 30 second window.
-const newsletterLimiter = rateLimit({
+export const newsletterLimiter = rateLimit({
     windowMs: 30000,
     max: 3,
     message: { msg: 'Too many submission. Please try again later.' },
     standardHeaders: true,
     legacyHeaders: false,
 });
-
-export default {contactFormLimiter, newsletterLimiter};
